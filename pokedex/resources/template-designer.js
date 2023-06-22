640bee63b428d1188134460cd8048f3621dc075f
Rishabh <lordeadroid@users.noreply.github.com>
2023-06-22 15:40:07 +0530

    refactor pokedex

--- pokedex/resources/template-designer.js
+++ pokedex/resources/template-designer.js
@@ -17,43 +17,43 @@ const main = () => {
 
     let data = context + `
     <div class="card">
-    <figure>
-    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${serial}.png" alt="${name}">
-    <figcaption>${name}</figcaption>
-    </figure>
-    <table class="table">
-    <tr>
-      <td class="attributes">Types</td>
-      <td class="value">${typesOf}</td>
-    </tr>
-    <tr>
-      <td class="attributes">Weight</td>
-      <td class="value">${weight}</td>
-    </tr>
-    <tr>
-      <td class="attributes">HP</td>
-      <td class="value">${hp}</td>
-    </tr>
-    <tr>
-      <td class="attributes">XP</td>
-      <td class="value">${xp}</td>
-    </tr>
-    <tr>
-      <td class="attributes">Attack</td>
-      <td class="value">${attack}</td>
-    </tr>
-    <tr>
-      <td class="attributes">Defense</td>
-      <td class="value">${defense}</td>
-    </tr>
-    </table>
-    </div >
+      <figure>
+      <div class="poke-image">
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${serial}.png" alt="${name}">
+      </div>
+      <figcaption>${name}</figcaption>
+      </figure>
+      <div class="attributes">
+        <div class="attribute">
+        <div class="property">Types</div>
+        <div class="value">${typesOf}</div>
+        </div>
+        <div class="attribute">
+        <div class="property">Weight</div>
+        <div class="value">${weight}</div>
+        </div>
+        <div class="attribute">
+        <div class="property">HP</div>
+        <div class="value">${hp}</div>
+        </div>
+        <div class="attribute">
+        <div class="property">XP</div>
+        <div class="value">${xp}</div>
+        </div>
+        <div class="attribute">
+        <div class="property">Attack</div>
+        <div class="value">${attack}</div>
+        </div>
+        <div class="attribute">
+        <div class="property">Defense</div>
+        <div class="value">${defense}</div>
+        </div>
+      </div>
+    </div>
   `
-    if ((index + 1) % 4 === 0) data += '</section><section class="row">';
     return data;
-  }, '<section class="row">');
-
-  fs.writeFileSync("template-stats", template);
+  }, "");
+  fs.writeFileSync("template", template);
 };
 
 main();
